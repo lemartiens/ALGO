11bdac3b9aeef553c560d9d5893bbee8698c1969
Othmane Hassani <othmane.hassani@fundingcircle.com>
2019-05-18 22:34:09 +0200

    first commit

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.lblcamx = New System.Windows.Forms.Label()
+        Me.lblcamy = New System.Windows.Forms.Label()
+        Me.lblzoom = New System.Windows.Forms.Label()
+        Me.lblH = New System.Windows.Forms.Label()
+        Me.lblL = New System.Windows.Forms.Label()
+        Me.boxJouer = New System.Windows.Forms.CheckBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.OptionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NouvellePartieToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lbl_OeufPondu = New System.Windows.Forms.Label()
+        Me.lbl_vie = New System.Windows.Forms.Label()
+        Me.lbl_burger = New System.Windows.Forms.Label()
+        Me.lbl_numBoid = New System.Windows.Forms.Label()
+        Me.VScrollBar1 = New System.Windows.Forms.VScrollBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.checkBoid = New System.Windows.Forms.CheckBox()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 24
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(863, 37)
+        Me.Chart1.Name = "Chart1"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.Chart1.Series.Add(Series1)
+        Me.Chart1.Size = New System.Drawing.Size(462, 405)
+        Me.Chart1.TabIndex = 25
+        Me.Chart1.Text = "Chart1"
+        '
+        'lblcamx
+        '
+        Me.lblcamx.AutoSize = True
+        Me.lblcamx.Location = New System.Drawing.Point(1004, 282)
+        Me.lblcamx.Name = "lblcamx"
+        Me.lblcamx.Size = New System.Drawing.Size(39, 13)
+        Me.lblcamx.TabIndex = 26
+        Me.lblcamx.Text = "Label2"
+        '
+        'lblcamy
+        '
+        Me.lblcamy.AutoSize = True
+        Me.lblcamy.Location = New System.Drawing.Point(1004, 308)
+        Me.lblcamy.Name = "lblcamy"
+        Me.lblcamy.Size = New System.Drawing.Size(39, 13)
+        Me.lblcamy.TabIndex = 27
+        Me.lblcamy.Text = "Label3"
+        '
+        'lblzoom
+        '
+        Me.lblzoom.AutoSize = True
+        Me.lblzoom.Location = New System.Drawing.Point(435, 9)
+        Me.lblzoom.Name = "lblzoom"
+        Me.lblzoom.Size = New System.Drawing.Size(39, 13)
+        Me.lblzoom.TabIndex = 28
+        Me.lblzoom.Text = "Label2"
+        '
+        'lblH
+        '
+        Me.lblH.AutoSize = True
+        Me.lblH.Location = New System.Drawing.Point(1004, 365)
+        Me.lblH.Name = "lblH"
+        Me.lblH.Size = New System.Drawing.Size(39, 13)
+        Me.lblH.TabIndex = 29
+        Me.lblH.Text = "Label2"
+        '
+        'lblL
+        '
+        Me.lblL.AutoSize = True
+        Me.lblL.Location = New System.Drawing.Point(1004, 387)
+        Me.lblL.Name = "lblL"
+        Me.lblL.Size = New System.Drawing.Size(39, 13)
+        Me.lblL.TabIndex = 30
+        Me.lblL.Text = "Label2"
+        '
+        'boxJouer
+        '
+        Me.boxJouer.AutoSize = True
+        Me.boxJouer.Location = New System.Drawing.Point(677, 7)
+        Me.boxJouer.Name = "boxJouer"
+        Me.boxJouer.Size = New System.Drawing.Size(52, 17)
+        Me.boxJouer.TabIndex = 31
+        Me.boxJouer.Text = "Jouer"
+        Me.boxJouer.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.WindowText
+        Me.PictureBox1.BackgroundImage = Global.WindowsApplication2.My.Resources.Resources.aquarium
+        Me.PictureBox1.Location = New System.Drawing.Point(10, 30)
+        Me.PictureBox1.MaximumSize = New System.Drawing.Size(800, 500)
+        Me.PictureBox1.MinimumSize = New System.Drawing.Size(800, 500)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(800, 500)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.LargeChange = 40
+        Me.HScrollBar1.Location = New System.Drawing.Point(10, 533)
+        Me.HScrollBar1.Maximum = 500
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(800, 20)
+        Me.HScrollBar1.SmallChange = 20
+        Me.HScrollBar1.TabIndex = 32
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OptionToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1362, 24)
+        Me.MenuStrip1.TabIndex = 33
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'OptionToolStripMenuItem
+        '
+        Me.OptionToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NouvellePartieToolStripMenuItem})
+        Me.OptionToolStripMenuItem.Name = "OptionToolStripMenuItem"
+        Me.OptionToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.OptionToolStripMenuItem.Text = "Option"
+        '
+        'NouvellePartieToolStripMenuItem
+        '
+        Me.NouvellePartieToolStripMenuItem.Name = "NouvellePartieToolStripMenuItem"
+        Me.NouvellePartieToolStripMenuItem.Size = New System.Drawing.Size(163, 22)
+        Me.NouvellePartieToolStripMenuItem.Text = "Nouvelle Partie..."
+        '
+        'lbl_OeufPondu
+        '
+        Me.lbl_OeufPondu.AutoSize = True
+        Me.lbl_OeufPondu.Location = New System.Drawing.Point(985, 501)
+        Me.lbl_OeufPondu.Name = "lbl_OeufPondu"
+        Me.lbl_OeufPondu.Size = New System.Drawing.Size(39, 13)
+        Me.lbl_OeufPondu.TabIndex = 34
+        Me.lbl_OeufPondu.Text = "Label2"
+        '
+        'lbl_vie
+        '
+        Me.lbl_vie.AutoSize = True
+        Me.lbl_vie.Location = New System.Drawing.Point(985, 473)
+        Me.lbl_vie.Name = "lbl_vie"
+        Me.lbl_vie.Size = New System.Drawing.Size(39, 13)
+        Me.lbl_vie.TabIndex = 35
+        Me.lbl_vie.Text = "Label2"
+        '
+        'lbl_burger
+        '
+        Me.lbl_burger.AutoSize = True
+        Me.lbl_burger.Location = New System.Drawing.Point(985, 533)
+        Me.lbl_burger.Name = "lbl_burger"
+        Me.lbl_burger.Size = New System.Drawing.Size(39, 13)
+        Me.lbl_burger.TabIndex = 36
+        Me.lbl_burger.Text = "Label2"
+        '
+        'lbl_numBoid
+        '
+        Me.lbl_numBoid.AutoSize = True
+        Me.lbl_numBoid.Location = New System.Drawing.Point(985, 445)
+        Me.lbl_numBoid.Name = "lbl_numBoid"
+        Me.lbl_numBoid.Size = New System.Drawing.Size(39, 13)
+        Me.lbl_numBoid.TabIndex = 37
+        Me.lbl_numBoid.Text = "Label2"
+        '
+        'VScrollBar1
+        '
+        Me.VScrollBar1.LargeChange = 40
+        Me.VScrollBar1.Location = New System.Drawing.Point(813, 30)
+        Me.VScrollBar1.Name = "VScrollBar1"
+        Me.VScrollBar1.Size = New System.Drawing.Size(23, 500)
+        Me.VScrollBar1.SmallChange = 20
+        Me.VScrollBar1.TabIndex = 38
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(322, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 21)
+        Me.Button1.TabIndex = 41
+        Me.Button1.Text = "Zoom par défaut"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'checkBoid
+        '
+        Me.checkBoid.AutoSize = True
+        Me.checkBoid.Location = New System.Drawing.Point(735, 7)
+        Me.checkBoid.Name = "checkBoid"
+        Me.checkBoid.Size = New System.Drawing.Size(75, 17)
+        Me.checkBoid.TabIndex = 42
+        Me.checkBoid.Text = "Jouer boid"
+        Me.checkBoid.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1362, 566)
+        Me.Controls.Add(Me.checkBoid)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.VScrollBar1)
+        Me.Controls.Add(Me.lbl_numBoid)
+        Me.Controls.Add(Me.lbl_burger)
+        Me.Controls.Add(Me.lbl_vie)
+        Me.Controls.Add(Me.lbl_OeufPondu)
+        Me.Controls.Add(Me.HScrollBar1)
+        Me.Controls.Add(Me.boxJouer)
+        Me.Controls.Add(Me.lblL)
+        Me.Controls.Add(Me.lblH)
+        Me.Controls.Add(Me.lblzoom)
+        Me.Controls.Add(Me.lblcamy)
+        Me.Controls.Add(Me.lblcamx)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Chart1 As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents lblcamx As System.Windows.Forms.Label
+    Friend WithEvents lblcamy As System.Windows.Forms.Label
+    Friend WithEvents lblzoom As System.Windows.Forms.Label
+    Friend WithEvents lblH As System.Windows.Forms.Label
+    Friend WithEvents lblL As System.Windows.Forms.Label
+    Friend WithEvents boxJouer As System.Windows.Forms.CheckBox
+    Friend WithEvents HScrollBar1 As System.Windows.Forms.HScrollBar
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents OptionToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NouvellePartieToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lbl_OeufPondu As System.Windows.Forms.Label
+    Friend WithEvents lbl_vie As System.Windows.Forms.Label
+    Friend WithEvents lbl_burger As System.Windows.Forms.Label
+    Friend WithEvents lbl_numBoid As System.Windows.Forms.Label
+    Friend WithEvents VScrollBar1 As System.Windows.Forms.VScrollBar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents checkBoid As System.Windows.Forms.CheckBox
+
+End Class
---
+++ WindowsApplication2/Form2.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl_fermer = New System.Windows.Forms.Button()
+        Me.lbl_L = New System.Windows.Forms.TextBox()
+        Me.lbl_rmin = New System.Windows.Forms.TextBox()
+        Me.lbl_rmax = New System.Windows.Forms.TextBox()
+        Me.lbl_vmin = New System.Windows.Forms.TextBox()
+        Me.lbl_vmax = New System.Windows.Forms.TextBox()
+        Me.lbl_nbp = New System.Windows.Forms.TextBox()
+        Me.lbl_nbr = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.checkPonte = New System.Windows.Forms.CheckBox()
+        Me.labelf = New System.Windows.Forms.Label()
+        Me.lbl_H = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lbl_fermer
+        '
+        Me.lbl_fermer.Location = New System.Drawing.Point(189, 427)
+        Me.lbl_fermer.Name = "lbl_fermer"
+        Me.lbl_fermer.Size = New System.Drawing.Size(90, 22)
+        Me.lbl_fermer.TabIndex = 0
+        Me.lbl_fermer.Text = "Go !"
+        Me.lbl_fermer.UseVisualStyleBackColor = True
+        '
+        'lbl_L
+        '
+        Me.lbl_L.Location = New System.Drawing.Point(250, 82)
+        Me.lbl_L.Name = "lbl_L"
+        Me.lbl_L.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_L.TabIndex = 1
+        Me.lbl_L.Text = "1000"
+        '
+        'lbl_rmin
+        '
+        Me.lbl_rmin.Location = New System.Drawing.Point(250, 108)
+        Me.lbl_rmin.Name = "lbl_rmin"
+        Me.lbl_rmin.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_rmin.TabIndex = 2
+        Me.lbl_rmin.Text = "10"
+        '
+        'lbl_rmax
+        '
+        Me.lbl_rmax.Location = New System.Drawing.Point(250, 134)
+        Me.lbl_rmax.Name = "lbl_rmax"
+        Me.lbl_rmax.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_rmax.TabIndex = 3
+        Me.lbl_rmax.Text = "20"
+        '
+        'lbl_vmin
+        '
+        Me.lbl_vmin.Location = New System.Drawing.Point(250, 160)
+        Me.lbl_vmin.Name = "lbl_vmin"
+        Me.lbl_vmin.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_vmin.TabIndex = 4
+        Me.lbl_vmin.Text = "5"
+        '
+        'lbl_vmax
+        '
+        Me.lbl_vmax.Location = New System.Drawing.Point(250, 186)
+        Me.lbl_vmax.Name = "lbl_vmax"
+        Me.lbl_vmax.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_vmax.TabIndex = 5
+        Me.lbl_vmax.Text = "10"
+        '
+        'lbl_nbp
+        '
+        Me.lbl_nbp.Location = New System.Drawing.Point(250, 212)
+        Me.lbl_nbp.Name = "lbl_nbp"
+        Me.lbl_nbp.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_nbp.TabIndex = 6
+        Me.lbl_nbp.Text = "20"
+        '
+        'lbl_nbr
+        '
+        Me.lbl_nbr.Location = New System.Drawing.Point(250, 238)
+        Me.lbl_nbr.Name = "lbl_nbr"
+        Me.lbl_nbr.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_nbr.TabIndex = 7
+        Me.lbl_nbr.Text = "5"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(190, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Veuillez choisir les paramètres suivant :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(101, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Largeur de l'aquarium :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(101, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(136, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Rayon minimum des Boids :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(101, 137)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(139, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Rayon maximum des Boids :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(101, 160)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(139, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Vitesse minimum des Boids :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(101, 186)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(142, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Vitesse maximum des Boids :"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(101, 215)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(105, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Nombre de Poisson :"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(101, 241)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(119, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Nombre de Prédateurs :"
+        '
+        'checkPonte
+        '
+        Me.checkPonte.AutoSize = True
+        Me.checkPonte.Location = New System.Drawing.Point(250, 290)
+        Me.checkPonte.Name = "checkPonte"
+        Me.checkPonte.Size = New System.Drawing.Size(60, 17)
+        Me.checkPonte.TabIndex = 16
+        Me.checkPonte.Text = "Pondre"
+        Me.checkPonte.UseVisualStyleBackColor = True
+        '
+        'labelf
+        '
+        Me.labelf.AutoSize = True
+        Me.labelf.Location = New System.Drawing.Point(101, 59)
+        Me.labelf.Name = "labelf"
+        Me.labelf.Size = New System.Drawing.Size(116, 13)
+        Me.labelf.TabIndex = 18
+        Me.labelf.Text = "Hauteur de l'aquarium :"
+        '
+        'lbl_H
+        '
+        Me.lbl_H.Location = New System.Drawing.Point(250, 56)
+        Me.lbl_H.Name = "lbl_H"
+        Me.lbl_H.Size = New System.Drawing.Size(100, 20)
+        Me.lbl_H.TabIndex = 17
+        Me.lbl_H.Text = "500"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 461)
+        Me.Controls.Add(Me.labelf)
+        Me.Controls.Add(Me.lbl_H)
+        Me.Controls.Add(Me.checkPonte)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl_nbr)
+        Me.Controls.Add(Me.lbl_nbp)
+        Me.Controls.Add(Me.lbl_vmax)
+        Me.Controls.Add(Me.lbl_vmin)
+        Me.Controls.Add(Me.lbl_rmax)
+        Me.Controls.Add(Me.lbl_rmin)
+        Me.Controls.Add(Me.lbl_L)
+        Me.Controls.Add(Me.lbl_fermer)
+        Me.MaximumSize = New System.Drawing.Size(500, 500)
+        Me.MinimumSize = New System.Drawing.Size(500, 500)
+        Me.Name = "Form2"
+        Me.Text = "Nouvelle Partie"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbl_fermer As System.Windows.Forms.Button
+    Friend WithEvents lbl_L As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_rmin As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_rmax As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_vmin As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_vmax As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_nbp As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_nbr As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents checkPonte As System.Windows.Forms.CheckBox
+    Friend WithEvents labelf As System.Windows.Forms.Label
+    Friend WithEvents lbl_H As System.Windows.Forms.TextBox
+End Class
